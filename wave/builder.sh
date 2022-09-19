2b49f0d2ebda63fab7a82c31c2d4d09cd72682dc
Hans-Andrew Gibbs <75492102+andymangibbs@users.noreply.github.com>
2022-09-19 11:32:25 -0400

    Update builder.sh

--- wave/builder.sh
+++ wave/builder.sh
@@ -5,8 +5,6 @@ cd /home/andy/go/src/wave_dependencies/wave/cli
 go build -o wv
 cd ../waved/cmd
 go build -o waved
-#cp this /etc/wave/
-#cd /home/andy/go/src/waveWithTimings/wave/waved/cmd
 cp /home/andy/go/src/wave_dependencies/wave/cli/wv /etc/wave/
 cp /home/andy/go/src/wave_dependencies/wave/waved/cmd/waved /etc/wave/
 #cd ../../storage/persistentserver/cmd/
